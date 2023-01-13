cc7efad5b1af512d32d9a38fd51d75387192af13
@manish8052 <manish@imcloud.co.kr>
2023-01-13 17:36:55 +0530

    Initial commit

--- requester/views.py
+++ requester/views.py
@@ -31,7 +31,6 @@ class RequesterAPI(APIView):
 class MyAssetAPI(APIView):
     def get(self, request, *args, **kwargs):
         requests = Requester.objects.all()
-        # requests = model_to_dict(requests).update({"status":"pending"})
         serializer = RequesterSerializer(requests, many=True)
         return Response(serializer.data)
 
@@ -39,25 +38,6 @@ class MyAssetAPI(APIView):
 class MatchedAssetAPI(APIView):
     def get(self, request, *args, **kwargs):
         requests = Requester.objects.raw('SELECT requester_requester.recipient, requester_requester.from_location, requester_requester.to_location, requester_requester.date, requester_requester.assets FROM requester_requester INNER JOIN rider_rider ON requester_requester.from_location = rider_rider.from_location AND requester_requester.to_location = rider_rider.to_location AND requester_requester.date = rider_rider.date')
-        # requests = model_to_dict(requests).update({"status":"pending"})
         serializer = RequesterSerializer(requests, many=True)
         return Response(serializer.data)
 
-# def create_AssetTransport(request):
-#     if request.method == 'POST':
-#         # Get the posted data
-#         from_location = request.POST.get('from_location')
-#         to_location = request.POST.get('to_location')
-#         date_time = request.POST.get('date_time')
-#         asset_type = request.POST.get('asset_type')
-#         sensitivities = request.POST.get('sensitivities')
-#         recipient = request.POST.get('recipient')
-#         # Create a new Employee object
-#         asset_transport = Requester(first_name=first_name, last_name=last_name, email=email)
-#         asset_transport.save()
-
-#         # Return a JSON response with the new employee's ID
-#         return JsonResponse({'employee_id': employee.id}, status=201)
-#     else:
-#         # Return a JSON response with an error message
-#         return JsonResponse({'error': 'Invalid request method'}, status=405)
